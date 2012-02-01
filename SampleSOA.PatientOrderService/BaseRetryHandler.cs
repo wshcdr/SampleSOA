8ad223c749a469007e698d864fed1cb7925c4fc7
Eric Kepes <eric.kepes@mckesson.com>
2012-02-01 14:52:53 -0500

    Cleaned up my packages. Damn you log4net! Oh, and experimented with a little message rety logic. Certainly not production-worthy, but interesting to know how it works.

---
+++ SampleSOA.PatientOrderService/BaseRetryHandler.cs
@@ -0,0 +1,22 @@
+﻿using MassTransit;
+
+namespace SampleSOA.PatientOrderService
+{
+    public abstract class BaseRetryHandler<TMessage> : Consumes<TMessage>.All 
+        where TMessage : class
+    {
+        public void Consume(TMessage message)
+        {
+            try
+            {
+                Handle(message);
+            }
+            catch
+            {
+                this.MessageContext().RetryLater();
+            }
+        }
+
+        protected abstract void Handle(TMessage message);
+    }
+}
--- SampleSOA.PatientOrderService/ItemPickedHandler.cs
+++ SampleSOA.PatientOrderService/ItemPickedHandler.cs
@@ -6,18 +6,30 @@ using SampleSOA.Messages;
 
 namespace SampleSOA.PatientOrderService
 {
-    public class ItemPickedHandler : Consumes<ItemPicked>.All
+    public class ItemPickedHandler : BaseRetryHandler<ItemPicked>
+        //: Consumes<ItemPicked>.All
     {
-        public void Consume(ItemPicked message)
+        private static readonly Random _random = new Random();
+
+        protected override void Handle(ItemPicked message)
         {
-            Console.WriteLine(
-                "[{0}] {1} Picked {2} units of item#{3} at {4} by {5}.",
-                message.CorrelationId,
-                message.EventTime,
-                message.Quantity,
-                message.ItemId,
-                message.AtDeviceId,
-                message.ByUserId);
+            if (_random.Next(3) > 0)
+            {
+                Console.WriteLine(
+                    "[{0}] {1} Picked {2} units of item#{3} at {4} by {5}.",
+                    message.CorrelationId,
+                    message.EventTime,
+                    message.Quantity,
+                    message.ItemId,
+                    message.AtDeviceId,
+                    message.ByUserId);
+            }
+            else
+            {
+                Console.WriteLine("Failing message {0}",
+                    message.CorrelationId);
+                throw new ArithmeticException("blah blah blah");
+            }
         }
     }
 }
